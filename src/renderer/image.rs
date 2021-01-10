8d0190c183031f6deb9f4b4a04b73519ef38871c
buyoh <japanjetone@live.jp>
2021-01-10 23:48:02 +0900

    refactor / button events

---
+++ src/renderer/image.rs
@@ -0,0 +1,93 @@
+use std::cell::*;
+use std::rc::*;
+
+// Our custom image type. This stores a heap allocated byte array for the pixels for each of our
+// images, can be sent safely between threads and can be temporarily converted to a Cairo image
+// surface for drawing operations
+#[derive(Clone)]
+pub struct Image {
+    width: i32,
+    height: i32,
+    data: Option<Box<[u8]>>,
+}
+
+impl Image {
+    // Creates a new, black image
+    pub fn new(width: i32, height: i32) -> Self {
+        Image {
+            width: width,
+            height: height,
+            data: Some(vec![0; 4 * width as usize * height as usize].into()),
+        }
+    }
+
+    // Calls the given closure with a temporary Cairo image surface. After the closure has returned
+    // there must be no further references to the surface.
+    pub fn with_surface<F: FnOnce(&cairo::ImageSurface)>(&mut self, func: F) {
+        // Helper struct that allows passing the pixels to the Cairo image surface and once the
+        // image surface is destroyed the pixels will be stored in the return_location.
+        //
+        // This allows us to give temporary ownership of the pixels to the Cairo surface and later
+        // retrieve them back in a safe way while ensuring that nothing else still has access to
+        // it.
+        struct ImageHolder {
+            image: Option<Box<[u8]>>,
+            return_location: Rc<RefCell<Option<Box<[u8]>>>>,
+        }
+
+        // This stores the pixels back into the return_location as now nothing
+        // references the pixels anymore
+        impl Drop for ImageHolder {
+            fn drop(&mut self) {
+                *self.return_location.borrow_mut() =
+                    Some(self.image.take().expect("Holding no image"));
+            }
+        }
+
+        // Needed for ImageSurface::create_for_data() to be able to access the pixels
+        impl AsRef<[u8]> for ImageHolder {
+            fn as_ref(&self) -> &[u8] {
+                self.image.as_ref().expect("Holding no image").as_ref()
+            }
+        }
+
+        impl AsMut<[u8]> for ImageHolder {
+            fn as_mut(&mut self) -> &mut [u8] {
+                self.image.as_mut().expect("Holding no image").as_mut()
+            }
+        }
+
+        // Temporary move out the pixels
+        let image = self.data.take().expect("Empty image");
+
+        // A new return location that is then passed to our helper struct below
+        let return_location = Rc::new(RefCell::new(None));
+        {
+            let holder = ImageHolder {
+                image: Some(image),
+                return_location: return_location.clone(),
+            };
+
+            // The surface will own the image for the scope of the block below
+            let surface = cairo::ImageSurface::create_for_data(
+                holder,
+                cairo::Format::Rgb24,
+                self.width,
+                self.height,
+                4 * self.width,
+            )
+            .expect("Can't create surface");
+            func(&surface);
+
+            // Now the surface will be destroyed and the pixels are stored in the return_location
+        }
+
+        // And here move the pixels back again
+        self.data = Some(
+            return_location
+                .borrow_mut()
+                .take()
+                .expect("Image not returned"),
+        );
+    }
+}
---
+++ src/renderer/mod.rs
@@ -0,0 +1,119 @@
+extern crate cairo;
+extern crate gio;
+extern crate gtk;
+
+use gio::prelude::*;
+use gtk::prelude::*;
+use std::cell::*;
+use std::rc::*;
+
+mod image;
+
+pub fn new_app_drawingarea() -> gtk::Frame {
+    let frame_builder = gtk::FrameBuilder::new();
+    let frame = frame_builder.build();
+
+    let builder = gtk::DrawingAreaBuilder::new().width_request(300);
+    let drawing_area = builder.build();
+
+    // note:
+    // キーイベントを取り出せない
+    // textarea系なら出来るのかな？windowから取れるので、今はそうしておく
+    // マウスは取り出せる
+    drawing_area
+        .add_events(gdk::EventMask::BUTTON_PRESS_MASK | gdk::EventMask::BUTTON_RELEASE_MASK);
+
+    drawing_area.connect_button_press_event(|_, event| {
+        println!(
+            "btnpress: {} ({}, {})",
+            event.get_button(),
+            event.get_position().0,
+            event.get_position().1
+        );
+        Inhibit(false)
+    });
+    drawing_area.connect_button_release_event(|_, event| {
+        println!(
+            "btnrelease: {} ({}, {})",
+            event.get_button(),
+            event.get_position().0,
+            event.get_position().1
+        );
+        Inhibit(false)
+    });
+
+    // note:
+    // drawing_area は worker_to_gui_rx.attach に所有権を奪われる。
+    // なので、frame に drawing_area をセットして、frameを返すことにしている。
+    // 何か間違っているかもしれない
+    frame.add(&drawing_area);
+
+    //
+
+    let mut initial_image = image::Image::new(400, 400);
+
+    initial_image.with_surface(|surface| {
+        let cr = cairo::Context::new(surface);
+        cr.set_source_rgb(0., 1., 0.);
+        cr.paint();
+    });
+    //
+    // This is the channel for sending results from the worker thread to the main thread
+    // For every received image, queue the corresponding part of the DrawingArea for redrawing
+    let (worker_to_gui_tx, worker_to_gui_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
+
+    let (to_worker_tx, to_worker_rx) = std::sync::mpsc::channel::<RefCell<image::Image>>();
+
+    // animation thread
+    // std::thread::spawn(glib::clone!(move || {
+    std::thread::spawn(move || {
+        let mut n = 0;
+        for image in to_worker_rx.iter() {
+            n = (n + 1) % 100;
+
+            // Draw an arc with a weirdly calculated radius
+            image.borrow_mut().with_surface(|surface| {
+                let context = cairo::Context::new(surface);
+                context.set_source_rgb(1.0, 1.0, 1.0);
+                context.paint();
+                context.set_source_rgb(0.0, 0.0, n as f64 / 100.0);
+                context.rectangle(50.0, 50.0, 100.0, 100.0);
+                context.stroke();
+                surface.flush();
+            });
+
+            // Send the finished image back to the GUI thread
+            let _ = worker_to_gui_tx.send(image);
+            std::thread::sleep(std::time::Duration::from_millis(15));
+        }
+    });
+
+    let buffer_image = Rc::new(RefCell::new(initial_image.clone()));
+
+    drawing_area.connect_draw(
+      glib::clone!(@weak buffer_image => @default-return Inhibit(false), move|_ /* widget */, context: &cairo::Context| {
+          buffer_image.borrow_mut().with_surface(|surface| {
+                context.set_source_surface(surface, 0.0, 0.0);
+                context.paint();
+                context.set_source_rgb(0.0, 0.0, 0.0);
+            });
+
+            Inhibit(false)
+        }),
+    );
+
+    let _ = to_worker_tx.send(RefCell::new(initial_image));
+
+    worker_to_gui_rx.attach(None, move |image| {
+        // Swap the newly received image with the old stored one and send the old one back to
+        // the worker thread
+        let next_image = image;
+        buffer_image.swap(&next_image);
+        let _ = to_worker_tx.send(next_image);
+
+        drawing_area.queue_draw_area(0, 0, 400, 400); // TODO:
+
+        Continue(true)
+    });
+    frame
+}
